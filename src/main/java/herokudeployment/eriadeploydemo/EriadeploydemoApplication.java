17b27e98e694b3e57ed7f41173c3f81eab558a61
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2023-10-07 18:59:44 -0500

    initial commit

---
+++ src/main/java/herokudeployment/eriadeploydemo/EriadeploydemoApplication.java
@@ -0,0 +1,13 @@
+package herokudeployment.eriadeploydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EriadeploydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EriadeploydemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/herokudeployment/eriadeploydemo/EriadeploydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package herokudeployment.eriadeploydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EriadeploydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
